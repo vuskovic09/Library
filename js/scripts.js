1959eadedb5a577b90003fa6d50a0c14eaf9cb3d
Filip Vušković <vuskovic09@users.noreply.github.com>
2019-01-26 18:17:29 +0100

    Half of first page

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,18 @@
 $( document ).ready(function() {
     console.log( "ready!" );
+
+    var topFiveIMG = $('.topFiveImg');
+    var autorNaslov = ["Žarko Laušević - Sve prođe, pa i doživotna", "Goran Marković - Beogradski trio", "Fredrik Bakman - Čovek po imenu Uve", "Dušan Radović - Ženski razgovori", "Vuk Drašković - Aleksandar od Jugoslavije"];
+    var prefix = "topFive";
+    for (var i = 0; i < topFiveIMG.length; i++) {
+      console.log(i);
+      topFiveIMG[i].innerHTML = "<img class = 'topFiveImage' src = images/" + prefix + [i+1] + ".jpg /><p class ='topFiveDesc'>" + autorNaslov[i] + "</p>"
+    }
+
+
+
+    $('#register').click(function(){
+      $('.registerForm').slideDown("slow",function(){
+      });
+    });
 });
